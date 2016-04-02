f277a2e7ee4b7e74f59fe955856dbe8956208518
serarmatloob <serarmatloob@oakland.edu>
2016-04-02 00:24:09 -0400

    updates

--- cse231/source/edu/oakland/helper/Node.java
+++ cse231/source/edu/oakland/helper/Node.java
@@ -6,21 +6,35 @@ package edu.oakland.helper;
  */
 public class Node {
 
-    public int key;
-    public int value;
-    public Node leftChild;
-    public Node rightChild;
+    private int key;
+    private int data;
+    private Node leftChild;
+    private Node rightChild;
     
-    public Node(int key, int value){
+    public Node(int key, int data){
         this.key= key;
-        this.value=value;
+        this.data=data;
     }
-    @Override
+    public int getKey(){
+    	return key;
+    }
+    public int getData(){
+        return data;
+    }
+    public Node getLeftChild(){
+    	return leftChild;
+    }
+    public Node getRightChild(){
+    	return rightChild;
+    }
+    public void setRightChild(Node leftNode){
+    	this.leftChild=leftNode;
+    }
+    public void setLeftChild(Node rightNode){
+    	this.rightChild=rightNode;
+    }
+     @Override
     public String toString(){
-        return value +" has the key "+key;
+        return data +" Which has the key "+key;
     }
-    public int getValue(){
-        return value;
-    }
-    
 }
